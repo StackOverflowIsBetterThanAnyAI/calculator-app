c8c9f2ddd00c90243b9463f30d1f614f551bc612
StackOverflowIsBetterThanAnyAI <michael.muenzenhofer@gmail.com>
2023-09-20 19:05:39 +0200

    done with logic

--- src/components/CalculatorLogic/CalculatorLogic.tsx
+++ src/components/CalculatorLogic/CalculatorLogic.tsx
@@ -1,7 +1,9 @@
 // checks if the user is allowed to type '0' or not
 export const checkForStartingZero = (displayedText: string | null): string => {
-    const splitDisplayedText = displayedText?.split(' ') || ''
-    const numberToCheck = splitDisplayedText ? splitDisplayedText.length - 1 : 0
+    const splitDisplayedText: string[] | '' = displayedText?.split(' ') || ''
+    const numberToCheck: number = splitDisplayedText
+        ? splitDisplayedText.length - 1
+        : 0
 
     // the latest set of numbers is checked if it starts with a '0' and a Comma
     for (
@@ -45,8 +47,8 @@ export const allowCommaUsage = (displayedText: string | null): boolean => {
     // no Comma is allowed in the beginning of a new set of numbers
     if (!displayedText) return false
 
-    const splitDisplayedText = displayedText.split(' ')
-    const numberToCheck = splitDisplayedText.length - 1
+    const splitDisplayedText: string[] = displayedText.split(' ')
+    const numberToCheck: number = splitDisplayedText.length - 1
 
     if (!splitDisplayedText[numberToCheck].length) return false
     for (let i = 0; i < splitDisplayedText[numberToCheck].length; i++) {
@@ -63,11 +65,11 @@ export const addArithmeticOperator = (
 ): string => {
     // in case there is no number after a Comma,
     // a '0' is added right in front of the arithmetic operator
-    const lastCharIsComma =
+    const lastCharIsComma: boolean =
         displayedText?.charAt(displayedText.length - 1) === ','
-    const addSpace =
+    const addSpace: '' | ' ' =
         displayedText?.charAt(displayedText.length - 1) === ' ' ? '' : ' '
-    const returnText = lastCharIsComma
+    const returnText: string = lastCharIsComma
         ? `0 ${buttonText} `
         : `${addSpace}${buttonText} `
     return displayedText &&
@@ -86,7 +88,7 @@ export const calculateLeftParantheses = (
     displayedText: string | null
 ): number => {
     if (!displayedText) return -1
-    let counterLeftParantheses = 0
+    let counterLeftParantheses: number = 0
     for (let i = 0; i < displayedText.length; i++) {
         if (displayedText.charAt(i) === '(') counterLeftParantheses++
     }
@@ -98,7 +100,7 @@ export const calculateRightParantheses = (
     displayedText: string | null
 ): number => {
     if (!displayedText) return -1
-    let counterRightParantheses = 0
+    let counterRightParantheses: number = 0
     for (let i = 0; i < displayedText.length; i++) {
         if (displayedText.charAt(i) === ')') counterRightParantheses++
     }
@@ -115,7 +117,7 @@ export const removeSetOfParantheses = (
             splitText[splitText.length - 1].charAt(i) === '(' &&
             splitText[splitText.length - 1].charAt(i + 1) === ')'
         ) {
-            const newSplitText = splitText
+            const newSplitText: string[] = splitText
             newSplitText[splitText.length - 1] =
                 splitText[splitText.length - 1].substring(0, i) +
                 splitText[splitText.length - 1].substring(i + 2)
@@ -131,11 +133,30 @@ export const calculateResult = (displayedText: string | null): string => {
     if (!displayedText) return ''
 
     // array which contains every set of numbers
-    let splitText = displayedText.split(' ')
+    let splitText: string[] = displayedText.split(' ')
 
     // first calculation if the array contains parantheses, points or dashes
-    let calculationContent = returnCalculationContent(splitText)
+    let calculationContent: {
+        parantheses: boolean
+        points: boolean
+        dashes: boolean
+    } = returnCalculationContent(splitText)
 
+    // first solve parantheses
+    if (
+        calculationContent.parantheses &&
+        !calculationContent.points &&
+        !calculationContent.dashes
+    )
+        return displayedText.replaceAll('(', '').replaceAll(')', '')
+
+    if (calculationContent.parantheses)
+        displayedText = recursiveParanthesesCalculation(displayedText)
+
+    splitText = displayedText.split(' ')
+    calculationContent = returnCalculationContent(splitText)
+
+    // then solve points
     if (calculationContent.points && !calculationContent.parantheses) {
         displayedText = recursivePointCalculation(splitText).join(' ')
     }
@@ -143,6 +164,7 @@ export const calculateResult = (displayedText: string | null): string => {
     splitText = displayedText.split(' ')
     calculationContent = returnCalculationContent(splitText)
 
+    // then solve dashes
     if (calculationContent.dashes && !calculationContent.parantheses) {
         displayedText = solveDashCalculation(splitText)
     }
@@ -150,10 +172,58 @@ export const calculateResult = (displayedText: string | null): string => {
     return displayedText
 }
 
+// solves the parantheses calculations into easier input
+const recursiveParanthesesCalculation = (displayedText: string): string => {
+    // 1) scan displayedText for the first opening paranthesis
+    // 2) scan displayedText for the first closing paranthesis
+    // 3) if there is another opening paranthesis before the first closing paranthesis, the new one is the selected one
+    // 4) if the first closing paranthesis has been found
+    // - extract the calculation
+    // - calculate the result
+    // - insert it back
+    // - and remove both parantheses
+    // 5) repeat steps 1) to 4) until there are no parantheses left
+    let indexOpeningParanthesis: number = 0
+    let indexClosingParanthesis: number = 0
+    // find the first opening and closing paranthesis
+    for (let i = 0; i < displayedText.length; i++) {
+        if (displayedText.charAt(i) === '(') {
+            indexOpeningParanthesis = i
+            continue
+        }
+        if (displayedText.charAt(i) === ')') {
+            indexClosingParanthesis = i
+            break
+        }
+    }
+
+    if (indexClosingParanthesis > 0) {
+        const insideParantheses: string = displayedText.slice(
+            indexOpeningParanthesis + 1,
+            indexClosingParanthesis
+        )
+        const splitText: string[] = insideParantheses.split(' ')
+        const pointSolved: string[] = recursivePointCalculation(splitText)
+        const dashSolved: string = solveDashCalculation(pointSolved)
+
+        // return the calculated result back into the string
+        const updatedDisplayedText: string =
+            displayedText.slice(0, indexOpeningParanthesis) +
+            dashSolved +
+            displayedText.slice(indexClosingParanthesis + 1)
+
+        // recursively process the remaining input
+        return recursiveParanthesesCalculation(updatedDisplayedText)
+    }
+    return displayedText
+}
+
 // as long as there is a multiplication or division sign in the array,
 // this function is calling itself and removes the signs by calculating results step by step
 const recursivePointCalculation = (splitText: string[]): string[] => {
-    const pointSolvedCalculation = []
+    if (splitText.length === 1) return splitText
+
+    const pointSolvedCalculation: string[] = []
     let prevMultAdded: boolean = false
     for (let i = 1; i < splitText?.length; i += 2) {
         if (['x', '/'].includes(splitText[i])) {
@@ -212,8 +282,8 @@ const recursivePointCalculation = (splitText: string[]): string[] => {
         }
     }
 
-    // enything after + or - is cut off, but only when there is a x or / before the first + or - and if there are at least two x's or /'s
-    let recursiveResult
+    // anything after + or - is cut off, but only when there is a x or / before the first + or - and if there are at least two x's or /'s
+    let recursiveResult: string[]
     for (let i = 0; i < pointSolvedCalculation?.length; i++) {
         if (['x', '/'].includes(pointSolvedCalculation[i])) {
             recursiveResult = recursivePointCalculation([
@@ -245,7 +315,8 @@ const solvePointCalculation = (
 
 // solves dashes only calculations
 const solveDashCalculation = (splitText: string[]): string => {
-    let result = 0
+    if (splitText.length === 1) return splitText[0]
+    let result: number = 0
 
     for (let i = 0; i < splitText?.length; i++) {
         if (i <= splitText?.length - 2) {
