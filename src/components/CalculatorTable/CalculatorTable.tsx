601e61ba10bfcd98d5a8788c01bb5e41013da57d
StackOverflowIsBetterThanAnyAI <michael.muenzenhofer@gmail.com>
2024-07-16 11:13:02 +0200

    order of constants and tpyes

--- src/components/CalculatorTable/CalculatorTable.tsx
+++ src/components/CalculatorTable/CalculatorTable.tsx
@@ -20,6 +20,31 @@ const COLOR_NUMBERS: string = '#f8b8a5'
 const COLOR_SYMBOLS: string = '#f19e5b'
 const COLOR_EQUALS: string = '#d66658'
 
+type tableCharacterProps = [string | number, string]
+
+const tableCharacters: tableCharacterProps[] = [
+    ['AC', COLOR_SYMBOLS],
+    ['/', COLOR_SYMBOLS],
+    ['x', COLOR_SYMBOLS],
+    ['DEL', COLOR_SYMBOLS],
+    [7, COLOR_NUMBERS],
+    [8, COLOR_NUMBERS],
+    [9, COLOR_NUMBERS],
+    ['-', COLOR_SYMBOLS],
+    [4, COLOR_NUMBERS],
+    [5, COLOR_NUMBERS],
+    [6, COLOR_NUMBERS],
+    ['+', COLOR_SYMBOLS],
+    [1, COLOR_NUMBERS],
+    [2, COLOR_NUMBERS],
+    [3, COLOR_NUMBERS],
+    ['()', COLOR_SYMBOLS],
+    [0, COLOR_NUMBERS],
+    [',', COLOR_SYMBOLS],
+    ['+/-', COLOR_SYMBOLS],
+    ['=', COLOR_EQUALS],
+]
+
 const CalculatorTable: FC<Props> = ({
     setSessionStorageValueInput,
     setSessionStorageValueOutput,
@@ -406,31 +431,6 @@ const CalculatorTable: FC<Props> = ({
             )
         setSessionStorageValueInput(displayedText || '')
     }
-
-    type tableCharacterProps = [string | number, string]
-
-    const tableCharacters: tableCharacterProps[] = [
-        ['AC', COLOR_SYMBOLS],
-        ['/', COLOR_SYMBOLS],
-        ['x', COLOR_SYMBOLS],
-        ['DEL', COLOR_SYMBOLS],
-        [7, COLOR_NUMBERS],
-        [8, COLOR_NUMBERS],
-        [9, COLOR_NUMBERS],
-        ['-', COLOR_SYMBOLS],
-        [4, COLOR_NUMBERS],
-        [5, COLOR_NUMBERS],
-        [6, COLOR_NUMBERS],
-        ['+', COLOR_SYMBOLS],
-        [1, COLOR_NUMBERS],
-        [2, COLOR_NUMBERS],
-        [3, COLOR_NUMBERS],
-        ['()', COLOR_SYMBOLS],
-        [0, COLOR_NUMBERS],
-        [',', COLOR_SYMBOLS],
-        ['+/-', COLOR_SYMBOLS],
-        ['=', COLOR_EQUALS],
-    ]
 
     return (
         <div className="calculatorTable">
