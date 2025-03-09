f7c2ec91a84e762c792835aec00eb325ec004305
Michael Münzenhofer <michael.muenzenhofer@gmail.com>
2025-03-09 14:41:33 +0100

    fix #13: remove web-vitals

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import Calculator from './components/Calculator/Calculator'
-import reportWebVitals from './reportWebVitals'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
@@ -10,5 +9,3 @@ root.render(
         <Calculator />
     </React.StrictMode>
 )
-
-reportWebVitals()
