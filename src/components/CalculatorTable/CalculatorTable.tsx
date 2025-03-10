3669cc9811ee106138bab080050db19a0b4ad600
Michael Münzenhofer <michael.muenzenhofer@gmail.com>
2025-03-10 14:53:27 +0100

    outsource tableCharacters

--- src/components/CalculatorTable/CalculatorTable.tsx
+++ src/components/CalculatorTable/CalculatorTable.tsx
@@ -10,40 +10,12 @@ import { checkForClosingParanthesis } from '../../calculatorLogic/checkForClosin
 import { checkForDeletedSpace } from '../../calculatorLogic/checkForDeletedSpace'
 import { checkForStartingZero } from '../../calculatorLogic/checkForStartingZero'
 import { removeSetOfParantheses } from '../../calculatorLogic/removeSetOfParantheses'
+import { tableCharacters } from '../../constants/tableCharacters'
 
 type CalculatorTableProps = {
     setSessionStorageValueInput: (newValue: string) => void
     setSessionStorageValueOutput: (newValue: string) => void
 }
-
-const COLOR_NUMBERS: string = '#f8b8a5'
-const COLOR_SYMBOLS: string = '#f19e5b'
-const COLOR_EQUALS: string = '#d66658'
-
-type TableCharacterProps = [string | number, string]
-
-const tableCharacters: TableCharacterProps[] = [
-    ['AC', COLOR_SYMBOLS],
-    ['/', COLOR_SYMBOLS],
-    ['x', COLOR_SYMBOLS],
-    ['DEL', COLOR_SYMBOLS],
-    [7, COLOR_NUMBERS],
-    [8, COLOR_NUMBERS],
-    [9, COLOR_NUMBERS],
-    ['-', COLOR_SYMBOLS],
-    [4, COLOR_NUMBERS],
-    [5, COLOR_NUMBERS],
-    [6, COLOR_NUMBERS],
-    ['+', COLOR_SYMBOLS],
-    [1, COLOR_NUMBERS],
-    [2, COLOR_NUMBERS],
-    [3, COLOR_NUMBERS],
-    ['()', COLOR_SYMBOLS],
-    [0, COLOR_NUMBERS],
-    [',', COLOR_SYMBOLS],
-    ['+/-', COLOR_SYMBOLS],
-    ['=', COLOR_EQUALS],
-]
 
 const CalculatorTable: FC<CalculatorTableProps> = ({
     setSessionStorageValueInput,
---
+++ src/constants/tableCharacters.ts
@@ -0,0 +1,28 @@
+const COLOR_NUMBERS: string = '#f8b8a5'
+const COLOR_SYMBOLS: string = '#f19e5b'
+const COLOR_EQUALS: string = '#d66658'
+
+type TableCharacterProps = [string | number, string]
+
+export const tableCharacters: TableCharacterProps[] = [
+    ['AC', COLOR_SYMBOLS],
+    ['/', COLOR_SYMBOLS],
+    ['x', COLOR_SYMBOLS],
+    ['DEL', COLOR_SYMBOLS],
+    [7, COLOR_NUMBERS],
+    [8, COLOR_NUMBERS],
+    [9, COLOR_NUMBERS],
+    ['-', COLOR_SYMBOLS],
+    [4, COLOR_NUMBERS],
+    [5, COLOR_NUMBERS],
+    [6, COLOR_NUMBERS],
+    ['+', COLOR_SYMBOLS],
+    [1, COLOR_NUMBERS],
+    [2, COLOR_NUMBERS],
+    [3, COLOR_NUMBERS],
+    ['()', COLOR_SYMBOLS],
+    [0, COLOR_NUMBERS],
+    [',', COLOR_SYMBOLS],
+    ['+/-', COLOR_SYMBOLS],
+    ['=', COLOR_EQUALS],
+]
